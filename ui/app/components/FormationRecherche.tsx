9d4fa9713c8eb04a8bba65d7fbfc2f06d38c4f57
Ananda <ananda10@gmail.com>
2023-07-05 16:16:51 +0200

    fix: erreur sur les cfds

--- ui/app/components/FormationRecherche.tsx
+++ ui/app/components/FormationRecherche.tsx
@@ -22,7 +22,7 @@ export default async function FormationRercherche({ searchParams }: any) {
   return (
     <FormationRechercheResult
       searchParams={{
-        cfds: searchFormations.split(),
+        cfds: searchFormations.split(','),
         distance: searchDistance,
         postCode: searchPostCode,
       }}
